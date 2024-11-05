1cfb1160e67e22d6d68657df0e2346bc429f8907
VivekRanjan620 <143477763+VivekRanjan620@users.noreply.github.com>
2024-11-05 12:05:28 +0530

    deploys

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/Cheery-civil-website/', // Replace with your actual repository name
+  // other Vite configurations
+});
