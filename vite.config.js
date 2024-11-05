7af07072fd6e7a925fcde0174ee2437967317752
VivekRanjan620 <143477763+VivekRanjan620@users.noreply.github.com>
2024-11-05 12:22:08 +0530

    dep

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 
-export default defineConfig({
-  base: '/Cheery-civil-website/', // Replace with your actual repository name
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/Cheery-civil-website/' : '/',
   // other Vite configurations
-});
+}));
+
